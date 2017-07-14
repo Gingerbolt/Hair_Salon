ab0d8cc448fa2e1b535b706de013e12a8118e26c
Steve Spitz <steve@spitz.com>
2017-07-14 14:17:05 -0700

    Add delete method and test; falsified; passing

--- src/Stylist.php
+++ src/Stylist.php
@@ -83,7 +83,16 @@
 
         function delete()
         {
-
+            $executed = $GLOBALS['DB']->exec("DELETE FROM stylists WHERE id = {$this->getId()};");
+             if (!$executed) {
+                 return false;
+             }
+             $executed = $GLOBALS['DB']->exec("DELETE FROM clients WHERE stylist_match_id = {$this->getId()};");
+             if (!$executed) {
+                 return false;
+             } else {
+                 return true;
+             }
         }
     }
 ?>
